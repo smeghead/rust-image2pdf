7773a272f9f1fe1d1f51df6af7058c40c8efb5a8
smeghead <smeghead7@gmail.com>
2021-04-09 00:12:39 +0900

    add ImageParameter structure.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ extern crate printpdf;
 
 use getopts::Options;
 use printpdf::*;
-//use std::io::Cursor;
 use image::jpeg::JpegDecoder;
 use std::fs::File;
 use std::io::BufWriter;
@@ -48,22 +47,43 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     let doc = PdfDocument::empty("printpdf graphics test");
 
     let dpi = 300.0;
-    // currently, the only reliable file formats are bmp/jpeg/png
-    // this is an issue of the image library, not a fault of printpdf
 
     for image_path in config.image_paths.iter() {
-        let (page_index, layer_index) = doc.add_page(Mm(210.0), Mm(297.0),"Page 2, Layer 1");
-        let current_layer = doc.get_page(page_index).get_layer(layer_index);
         println!("image_path: {}", image_path);
-        let mut image_file = File::open(image_path).unwrap();
-        let decoder = JpegDecoder::new(&mut image_file).unwrap();
-        let image2 = Image::try_from(decoder).unwrap();
+        let param = ImageParameter::new(image_path.to_string()).unwrap();
 
-        println!("image2 width: {:?} height: {:?}", image2.image.width.into_pt(dpi), image2.image.height.into_pt(dpi));
-        image2.add_to_layer(current_layer.clone(), None, Some(Mm(100.0)), None, Some(2.0), Some(2.0), Some(dpi));
+        let (page_index, layer_index) = doc.add_page(Mm(param.page_width), Mm(param.page_height), "Page 2, Layer 1");
+        let current_layer = doc.get_page(page_index).get_layer(layer_index);
+        param.image.add_to_layer(
+            current_layer.clone(),
+            None,
+            Some(Mm(100.0)),
+            None,
+            Some(param.scale),
+            Some(param.scale),
+            Some(dpi));
     }
 
     doc.save(&mut BufWriter::new(File::create(config.output_filename).unwrap())).unwrap();
 
     return Ok(());
+}
+
+pub struct ImageParameter {
+    pub image: Image,
+    pub scale: f64,
+    pub page_width: f64,
+    pub page_height: f64,
+}
+impl ImageParameter {
+    pub fn new(filename: String) -> Result<ImageParameter, &'static str> {
+        let mut image_file: File = File::open(filename.clone()).unwrap();
+        let decoder = JpegDecoder::new(&mut image_file).unwrap();
+        let image = Image::try_from(decoder).unwrap();
+        let scale = 1.0; //TODO culcurate scale.
+
+        let page_width = 210.0; //TODO portrait or landscape
+        let page_height = 297.0; //TODO portrait or landscape
+        Ok(ImageParameter {image, scale, page_width, page_height})
+    }
 }
